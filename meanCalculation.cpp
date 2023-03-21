9edd32839b1948cc2c5c3f151ad5e5997e72fb91
zenas-awuku <zenas.awuku@turntabl.io>
2023-03-21 11:19:55 +0000

    update to mode

--- meanCalculation.cpp
+++ meanCalculation.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <vector>
 
 double mean(int array[], double size)
 {
@@ -15,23 +16,46 @@ double median(int array[], int size)
 {
 	if (size % 2 != 0)
 		return array[size / 2];
-	return (array[(size-1)/2]  + array[size/2])/2.0;
+	return (array[(size - 1) / 2] + array[size / 2]) / 2.0;
 }
 
-int mode(int array[], int size) {
-	std::map<int, int> frequencyMap;
+std::vector<int> mode(int array[], int size)
+{
+	std::unordered_map<int, int> frequencyMap;
 
 	for (int i = 0; i < size; i++)
 	{
-		if (frequencyMap[array[i]])
+		if (frequencyMap.find(array[i]) != 0)
 		{
 			frequencyMap[array[i]]++;
-		} else {
+		}
+		else
+		{
 			frequencyMap.insert({array[i], 1});
 		}
-		
 	}
-	
+
+	std::vector<int> modeList;
+	modeList.push_back(array[0]);
+
+	for (auto i : frequencyMap)
+	{
+		if (i.second < frequencyMap[modeList.back()])
+		{
+			continue;
+		}
+		else if (i.second == frequencyMap[modeList.back()])
+		{
+			
+			modeList.push_back(i.first);
+		}
+		else if (i.second > frequencyMap[modeList.back()]) {
+			modeList.clear();
+			modeList.push_back(i.first);
+		}
+	}
+
+	return modeList;
 }
 
 int main()
@@ -47,4 +71,10 @@ int main()
 	std::sort(array, array + arraySize);
 	std::cout << "The mean value is: " << mean(array, arraySize) << std::endl;
 	std::cout << "The median value is: " << median(array, arraySize) << std::endl;
+	std::cout << "The list of the mode values is: ";
+	std::vector<int> modeList = mode(array, arraySize);
+	for(int i; i < modeList.size(); i++) {
+		std::cout << modeList[i] << " ";
+	}
+	std::cout << std::endl;
 }
